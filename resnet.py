188fbce43110fd13d0e70574c323a80967d6b6bd
Saeshu <99483970+Saeshu@users.noreply.github.com>
2026-05-30 20:38:15 +0530

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -4,10 +4,7 @@ import torch.nn as nn
 def get_model():
     model = models.resnet18(pretrained=True)
 
-    # modify first layer for grayscale
     model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
-
-    # modify output
     model.fc = nn.Linear(model.fc.in_features, 1)
 
     return model
